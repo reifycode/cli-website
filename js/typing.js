8c99b828c5a760cc8443c46a8268f6e3bca72afe
ShreyashPatodia <patodiashreyash32@gmail.com>
2016-02-28 04:07:31 +1100

    Correct typing.js added

--- js/typing.js
+++ js/typing.js
@@ -1,16 +1,13 @@
-var comandLineText = 'root@root:~$ Hello';
+var commandLineText = "String data here";
 var textArray = commandLineText.split("");
 var loopTimer;
-function type()
-{
-	if(textArray.lenth > 0)
-	{
+function type() {
+	if(textArray.length > 0) {
 		document.getElementById("blurry-text").innerHTML += textArray.shift();
+	} else {
+		clearTimeout(loopTimer); 
+                return false;
 	}
-	else
-	{
-		clearTimeout(loopTimer);
-		return false;
-	}
-	loopTimer = setTimeout('type()', 50);
+	loopTimer = setTimeout('type()',70);
 }
+type();
