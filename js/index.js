2648313b5995b92c79e9ff143f47ac89f6679fb9
Pulkit Agarwal <thepulkitagarwal@gmail.com>
2016-02-28 12:14:06 +0530

    Refactor typing.js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+window.app.type('words here and there');
+window.app.type('words here and there');
+window.app.type('words here and there');
+window.app.type('words here and there');
+window.app.type('words here and there');
+window.app.type('another one');
--- js/typing.js
+++ js/typing.js
@@ -1,13 +1,22 @@
-var commandLineText = "String data here";
-var textArray = commandLineText.split("");
-var loopTimer;
-function type() {
-	if(textArray.length > 0) {
-		document.getElementById("user-text-active").innerHTML += textArray.shift();
-	} else {
-		clearTimeout(loopTimer); 
-                return false;
+(function() {
+	var _textArray = [];
+	var _shiftTime = 70; // Time to shift each letter
+	var _idToTypeAfter = "user-text-active";
+
+	function type(text) {
+		if(text) {
+			_textArray = _textArray.concat(text.split(""));
+			if(_textArray.length > text.length) {
+				return ;
+			}
+		}
+
+		if(_textArray.length > 0) {
+			document.getElementById(_idToTypeAfter).innerHTML += _textArray.shift();
+			setTimeout(type, _shiftTime);
+		}
 	}
-	loopTimer = setTimeout('type()', 70);
-}
-type();
+
+	window.app = window.app || {};
+	window.app.type = type;
+})()
