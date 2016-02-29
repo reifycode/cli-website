49342c7f7df12b3f01652eddb0b0e0b87b83dee9
ShreyashPatodia <patodiashreyash32@gmail.com>
2016-03-01 01:36:42 +1100

    time added onto the username + minor stylistic changes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
+window.app.type("Hello there visitor, looks like you have stumbled onto us during one of your ");
+window.app.type(" purposeless crusades across github. But now that you have already decided ");
+window.app.type("to spend so much time on looking at this project of ours. Check out some of our");
+window.app.type(" other projects or just go to our github page and follow us there.");
+/*window.app.type('words here and there');
 window.app.type('words here and there');
 window.app.type('words here and there');
 window.app.type('words here and there');
-window.app.type('words here and there');
-window.app.type('words here and there');
-window.app.type('another one');
+window.app.type('another one');*/
