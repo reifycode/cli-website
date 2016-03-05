67ba70b8f46298c4f6291dde4116c1cb7f6025c1
Pulkit Agarwal <thepulkitagarwal@gmail.com>
2016-03-05 17:14:23 +0530

    Fix multi-line printing effect

--- js/typing.js
+++ js/typing.js
@@ -1,9 +1,25 @@
 (function() {
 	var _textArray = [];
 	var _shiftTime = 70; // Time to shift each letter
-	var _idToTypeAfter = "user-text-active";
-	var _currentVariable;
+	var _userNameText = "-reify@root:~$ ";
 
+
+	var _cache = {
+		app: document.getElementById("app")
+	}
+
+	var _id = {
+		activeText: "user-text-active",
+		cursor: "blinking-cursor"
+	}
+
+	var _class = {
+		lineDiv: "line",
+		userName: "user-name",
+		userText: "user-text"
+	}
+
+	// aka the typing effect generator.
 	function type(text) {
 		if(text) {
 			_textArray = _textArray.concat(text.split(""));
@@ -12,22 +28,57 @@
 			}
 		}
 
-		if(_textArray.length > 0) 
-		{
-			_currentVariable = _textArray.shift();
-			if(_currentVariable == '\n')
-			{
-				document.getElementById(_idToTypeAfter).innerHTML += "<br/>";
-				document.getElementById(user-entry) = "-reify@root:~$ ";
+		if(_textArray.length > 0) {
+			var currentText = _textArray.shift();
 
-			}
-			else
-			{
-				document.getElementById(_idToTypeAfter).innerHTML += _currentVariable;
-			}
+			if(currentText == '\n') newLine();
+			else document.getElementById(_id.activeText).innerHTML += currentText;
+
 			setTimeout(type, _shiftTime);
 		}
 	}
+
+	function getTimeText() {
+		var checkTime = i => { return (i < 10) ? ("0" + i) : i; }
+
+		var today = new Date();
+		var h = today.getHours();
+		var m = today.getMinutes();
+		h = checkTime(h);
+		m = checkTime(m);
+		return h + ":" + m;
+	}
+
+	function newLine() {
+		var previousActive = document.getElementById(_id.activeText);
+		
+		// remove the blinking cursor
+		previousActive.parentNode.removeChild(previousActive.nextSibling);
+		// and then remove its status as active (we indicated status by id)
+		previousActive.removeAttribute("id");
+
+		var lineNode = document.createElement("DIV");
+		lineNode.className = _class.lineDiv;
+
+		// 00:00-reify@root:~$
+		var userNameNode = document.createElement("SPAN");
+		userNameNode.className = _class.userName;
+		userNameNode.appendChild(document.createTextNode(getTimeText() + _userNameText));
+		lineNode.appendChild(userNameNode);
+
+		// Whatever is being typed
+		var activeNode = document.createElement("SPAN");
+		activeNode.className = _class.userText;
+		activeNode.id = _id.activeText;
+		lineNode.appendChild(activeNode);
+
+		var cursor = document.createElement("SPAN");
+		cursor.id = _id.cursor;
+		lineNode.appendChild(cursor);
+
+		_cache.app.appendChild(lineNode);
+	}
+
 	window.app = window.app || {};
 	window.app.type = type;
 })()
